2e12cf030cbcdd181e59fbdc5e659a9554d4c02e
yoitete <kh.70239568@gmail.com>
2026-05-24 20:24:51 +0900

    新規作成ページの改修

--- new.php
+++ new.php
@@ -5,12 +5,12 @@
   <title>新規作成</title>
 </head>
 <body>
-  <form action="" method="post">
+  <form action="store.php" method="post">
     <input type="text" name="content">
     <input type="submit" value="作成">
   </form>
   <div>
-    <a href="">一覧へもどる</a>
+    <a href="index.html">一覧へもどる</a>
   </div>
 </body>
 </html>
