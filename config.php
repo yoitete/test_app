0dd835de74f566294919db639ff896512c475c05
yoitete <kh.70239568@gmail.com>
2026-05-24 22:14:06 +0900

    DB接続処理を追加

--- config.php
+++ config.php
@@ -16,4 +16,6 @@ function errorHandler($errNo, $errStr, $errFile, $errLine)
     }
 
     return false;
-}
+}define('DSN', 'mysql:dbname=php_lesson;host=localhost;unix_socket=/tmp/mysql.sock');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
